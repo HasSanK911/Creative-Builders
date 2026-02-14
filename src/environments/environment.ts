8cda26256b64a469a955a9480507fd16c93043d9
Ali Hassan <kaleemikennels@gmail.com>
2026-02-14 16:44:49 +0500

    auth service created

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   environmentName: 'development',
-  apiUrl: 'https://localhost:5555/api',
+  apiUrl: 'http://localhost:5555/api',
   CompanyName: 'Creative Builders',
 };
 
